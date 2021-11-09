3579777fa86bbfa03eceea2023a38a8a807593ce
James Andrews <jbandrew14@gmail.com>
2021-11-09 08:52:20 -0500

    created Book model, added book data from .csv using admin site, renamed .env.sample to .env so django site would run

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import DateTimeField
 
 
 class User(AbstractUser):
@@ -8,3 +9,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=500)
+    author = models.CharField(max_length=500)
+    description = models.TextField()
+    url = models.CharField(max_length=500)
+    created_at = DateTimeField(auto_now_add=True)
